d0aefc14aa16ad0289c112f07b1f589ef8e69b71
nootim <dev@nootim.io>
2020-12-13 17:09:48 +0100

    add test in user-detail and user-todo

--- src/app/components/user-details-todo/user-details-todo.component.spec.ts
+++ src/app/components/user-details-todo/user-details-todo.component.spec.ts
@@ -19,7 +19,36 @@ describe('UserDetailsTodoComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should toggle status when onTodoUpdateStatus is call', () => {
+
+    const todo = {
+      id: 1,
+      userId: 1,
+      completed: false,
+      title: 'title'
+    };
+
+    component.onTodoUpdateStatus(todo);
+
+    expect(todo.completed).toBe(true);
+  });
+
+  it('should emit todo when onTodoUpdateStatus is call', () => {
+    spyOn(component.updateStatus, 'emit');
+
+    const todo = {
+      id: 1,
+      userId: 1,
+      completed: false,
+      title: 'title'
+    };
+
+    component.onTodoUpdateStatus(todo);
+
+    expect(component.updateStatus.emit).toHaveBeenCalledWith(todo);
+  });
 });
--- src/app/components/user-details-user/user-details-user.component.spec.ts
+++ src/app/components/user-details-user/user-details-user.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { UserDetailsUserComponent } from './user-details-user.component';
 
@@ -22,4 +23,28 @@ describe('UserDetailsUserComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the label for a row', () => {
+    component.label = 'Address';
+
+    fixture.detectChanges();
+
+    const label = fixture.debugElement
+    .nativeElement.querySelector('.nootim-label');
+
+    expect(label.textContent).toBe('Address')
+
+  });
+
+  it('should display the value for a row', () => {
+    component.value = 'Eiffel Tower 1st floor';
+
+    fixture.detectChanges();
+
+    const value = fixture.debugElement
+    .nativeElement.querySelector('.nootim-value');
+
+    expect(value.textContent).toBe('Eiffel Tower 1st floor')
+
+  })
 });
